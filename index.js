0823165eda313a4d4367493d43c81883a8114a29
nmhjkhkhj <dsjcoctorgirl43@hotmail.com>
2024-05-06 01:23:21 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+};
