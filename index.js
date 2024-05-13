21886a58cfbc50cb1b5dfeed5bd1eb499abce8cb
nmhjkhkhj <dsjcoctorgirl43@hotmail.com>
2024-05-14 06:32:46 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const fibonacci = (n) => {
-  const sequence = [0, 1];
+function minCostClimbingStairs(cost) {
+  const n = cost.length;
+  const dp = new Array(n + 1).fill(0);
   for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
   }
-  return sequence;
-};
+  return dp[n];
+}
